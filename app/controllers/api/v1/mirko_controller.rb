9adfa0ae3a3a745959d5dbd23793ab4818e22fcc
mjavurek <57668028+mjavurek@users.noreply.github.com>
2025-03-12 16:57:29 +0100

    simplified and optimized database query

--- app/controllers/api/v1/mirko_controller.rb
+++ app/controllers/api/v1/mirko_controller.rb
@@ -3,26 +3,16 @@ class Api::V1::MirkoController < Api::V1::BaseController
 
   def index
     time_now = Time.now
-    gos = GroupOrder
-          .includes(group_order_articles: { order_article: :article },
-                    order: :supplier)
-          .references(:orders)
-          .where(ordergroup_id: current_user.ordergroup.id)
-          .limit(200)
-          .where(orders: {
-                   ends: (time_now.weeks_ago(1)..time_now.next_year), state: "open", pickup: nil
-                 })
-          .or(
-            GroupOrder
-              .includes(group_order_articles: { order_article: :article },
-                        order: :supplier)
-              .references(:orders)
-              .where(ordergroup_id: current_user.ordergroup.id)
-              .limit(200)
-              .where(orders: {
-                       pickup: ((time_now.weeks_ago(5)..(time_now.weeks_ago(-1)))), state: %w[finished received closed]
-                     })
-          )
+    time_range_open = time_now.weeks_ago(1)..time_now.next_year
+    time_range_closed = time_now.weeks_ago(5)..time_now.weeks_ago(-1)
+    common_scope = GroupOrder
+                 .includes(group_order_articles: { order_article: :article }, order: :supplier)
+                 .references(:orders)
+                 .where(ordergroup_id: current_user.ordergroup.id)
+                 .limit(200)
+    gos = common_scope.where(orders: { ends: time_range_open, state: "open", pickup: nil })
+            .or(common_scope.where(orders: { pickup: time_range_closed, state: %w[finished received closed] }))
+
     render json: gos, each_serializer: MirkoSerializer
   end
 end
